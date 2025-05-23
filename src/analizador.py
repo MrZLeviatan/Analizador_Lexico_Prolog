7ec75b2d95d46a460fd5c2f58829a4226e4ec804
Nicolas C.S <nikis281002@gmail.com>
2025-05-23 14:12:32 -0500

    Se implementa el analizador de las Palabras Reservadas
    Se implementa el token para analizar las  palabras reservadas del lenguaje Prolog

--- src/analizador.py
+++ src/analizador.py
@@ -1,93 +1,131 @@
 from tokens.numeros_naturales import es_numero_natural
 from tokens.numero_reales import es_numero_real
 from tokens.identificadores import es_variable, es_atomo
+from tokens.palabras_reservadas import es_palabra_reservada
 
 simbolos_validos = ['=', ',', '.', '(', ')', '{', '}', '+', '-', '*', '/', '<', '>', '!']
 
 def analizar_codigo(codigo: str):
     resultados = []
-    token = ""
-    tipo_token = None  # "numero", "variable", "atomo"
-    pos = 0
     i = 0
+    pos = 0
     longitud = len(codigo)
 
     while i < longitud:
-        char = codigo[i]
+        token = ""
+        j = i
+        detectado = False
 
-        if char.isalnum() or char == '_':
-            if token == "":
-                # Determinar el tipo al ver el primer caracter
-                if char.isdigit():
-                    tipo_token = "numero"
-                elif char == '_' or ('A' <= char <= 'Z'):
-                    tipo_token = "variable"
-                elif 'a' <= char <= 'z':
-                    tipo_token = "atomo"
-                token += char
-                i += 1
-            else:
-                if tipo_token == "numero" and not char.isdigit():
-                    # Se corta el número si hay letra
-                    resultados.append((token, "Número Natural", pos))
-                    pos += 1
-                    token = ""
-                    tipo_token = None
-                elif tipo_token == "variable" and not (char.isalnum() or char == '_'):
-                    resultados.append((token, "Variable", pos))
-                    pos += 1
-                    token = ""
-                    tipo_token = None
-                elif tipo_token == "atomo" and not (char.isalnum() or char == '_'):
-                    resultados.append((token, "Átomo", pos))
-                    pos += 1
-                    token = ""
-                    tipo_token = None
+        # Intentar detectar palabra reservada de izquierda a derecha
+        while j < longitud and (codigo[j].isalnum() or codigo[j] == '_'):
+            token += codigo[j]
+            if es_palabra_reservada(token):
+                resultados.append((token, "Palabra Reservada", pos))
+                pos += 1
+                i = j + 1
+                detectado = True
+                break
+            j += 1
+
+        if detectado:
+            continue
+
+        if token != "":
+            # Fragmentar secuencia tipo 23A2isnot
+            sub_i = 0
+            sub_token = ""
+            sub_tipo = None
+            while sub_i < len(token):
+                c = token[sub_i]
+                if sub_token == "":
+                    if c.isdigit():
+                        sub_tipo = "numero"
+                    elif c == '_' or ('A' <= c <= 'Z'):
+                        sub_tipo = "variable"
+                    elif 'a' <= c <= 'z':
+                        sub_tipo = "atomo"
+                    sub_token += c
                 else:
-                    token += char
-                    i += 1
-        elif char == '.':
-            if token.isdigit() and i + 1 < longitud and codigo[i + 1].isdigit():
-                token += '.'
-                i += 1
-                token += codigo[i]
-                i += 1
-                resultados.append((token, "Número Real", pos))
-                pos += 1
-                token = ""
-                tipo_token = None
-            else:
-                if token != "":
-                    resultados.append((token, categorizar_token(token), pos))
-                    pos += 1
-                    token = ""
-                    tipo_token = None
-                resultados.append(('.', "Símbolo", pos))
-                pos += 1
-                i += 1
-        else:
-            if token != "":
-                resultados.append((token, categorizar_token(token), pos))
-                pos += 1
-                token = ""
-                tipo_token = None
-            if char.strip() != "":
-                if char in simbolos_validos:
-                    resultados.append((char, "Símbolo", pos))
-                elif char.isdigit():
-                    resultados.append((char, "Número Natural", pos))
-                else:
-                    resultados.append((char, "Token no reconocido", pos))
-                pos += 1
+                    if sub_tipo == "numero" and not c.isdigit():
+                        resultados.append((sub_token, categorizar_token(sub_token), pos))
+                        pos += 1
+                        sub_token = ""
+                        sub_tipo = None
+                        continue
+                    elif sub_tipo in ["variable", "atomo"] and not (c.isalnum() or c == '_'):
+                        resultados.append((sub_token, categorizar_token(sub_token), pos))
+                        pos += 1
+                        sub_token = ""
+                        sub_tipo = None
+                        continue
+                    sub_token += c
+                sub_i += 1
+
+            if sub_token != "":
+                # Intentar detectar palabras reservadas dentro del resto
+                temp_i = 0
+                while temp_i < len(sub_token):
+                    acumulado = ""
+                    k = temp_i
+                    while k < len(sub_token):
+                        acumulado += sub_token[k]
+                        if es_palabra_reservada(acumulado):
+                            resultados.append((acumulado, "Palabra Reservada", pos))
+                            pos += 1
+                            temp_i = k + 1
+                            break
+                        k += 1
+                    else:
+                        # No es palabra reservada, construir token válido
+                        fragmento = ""
+                        while temp_i < len(sub_token) and not es_palabra_reservada(sub_token[temp_i:]):
+                            fragmento += sub_token[temp_i]
+                            temp_i += 1
+                        if fragmento != "":
+                            resultados.append((fragmento, categorizar_token(fragmento), pos))
+                            pos += 1
+                i = j
+                continue
+
+        if i < longitud and (codigo[i].isalnum() or codigo[i] == '_'):
             i += 1
+            continue
 
-    if token != "":
-        resultados.append((token, categorizar_token(token), pos))
+        if i < longitud:
+            c = codigo[i]
+            if c == '.':
+                if len(resultados) > 0 and resultados[-1][1] == "Número Natural" and i + 1 < longitud and codigo[i + 1].isdigit():
+                    anterior = resultados.pop()
+                    numero = anterior[0]
+                    i += 1
+                    decimal = ""
+                    while i < longitud and codigo[i].isdigit():
+                        decimal += codigo[i]
+                        i += 1
+                    resultados.append((numero + '.' + decimal, "Número Real", anterior[2]))
+                else:
+                    resultados.append(('.', "Símbolo", pos))
+                    i += 1
+                    pos += 1
+            elif c.strip() != "":
+                if c in simbolos_validos:
+                    resultados.append((c, "Símbolo", pos))
+                elif c.isdigit():
+                    resultados.append((c, "Número Natural", pos))
+                else:
+                    resultados.append((c, "Token no reconocido", pos))
+                i += 1
+                pos += 1
+            else:
+                i += 1  # espacios y tabulaciones
 
     return resultados
 
+
 def categorizar_token(token: str) -> str:
-    if es_numero_real(token):
+    if es_palabra_reservada(token):
+        return "Palabra Reservada"
+    elif es_numero_real(token):
         return "Número Real"
     elif es_numero_natural(token):
         return "Número Natural"
---
+++ src/tokens/palabras_reservadas.py
@@ -0,0 +1,92 @@
+
+def es_palabra_reservada(cadena: str) -> bool:
+    """
+    Verifica si una cadena es una palabra reservada en Prolog.
+    Solo se permite usar if/else y comparación carácter por carácter.
+
+    Ejemplo: is / not / halt
+    """
+
+    longitud = len(cadena)  # Obtener la longitud de la cadena
+
+    # Palabra reservada: "is"
+    if longitud == 2:
+        if cadena[0] == 'i':  # Primera letra debe ser 'i'
+            if cadena[1] == 's':  # Segunda letra debe ser 's'
+                return True  # Es palabra reservada
+
+    # Palabra reservada: "not"
+    elif longitud == 3:
+        if cadena[0] == 'n':  # Primera letra 'n'
+            if cadena[1] == 'o':  # Segunda letra 'o'
+                if cadena[2] == 't':  # Tercera letra 't'
+                    return True  # Es palabra reservada
+
+    # Palabras reservadas de longitud 4: "true", "fail", "halt"
+    elif longitud == 4:
+        if cadena[0] == 't':  # Primera letra 't'
+            if cadena[1] == 'r':  # Segunda letra 'r'
+                if cadena[2] == 'u':  # Tercera letra 'u'
+                    if cadena[3] == 'e':  # Cuarta letra 'e'
+                        return True  # Es palabra reservada "true"
+        else:
+            if cadena[0] == 'f':  # Primera letra 'f'
+                if cadena[1] == 'a':  # Segunda letra 'a'
+                    if cadena[2] == 'i':  # Tercera letra 'i'
+                        if cadena[3] == 'l':  # Cuarta letra 'l'
+                            return True  # Es palabra reservada "fail"
+            else:
+                if cadena[0] == 'h':  # Primera letra 'h'
+                    if cadena[1] == 'a':  # Segunda letra 'a'
+                        if cadena[2] == 'l':  # Tercera letra 'l'
+                            if cadena[3] == 't':  # Cuarta letra 't'
+                                return True  # Es palabra reservada "halt"
+
+    # Palabras reservadas de longitud 6: "repeat", "assert", "clause"
+    elif longitud == 6:
+        if cadena[0] == 'r':  # Primera letra 'r'
+            if cadena[1] == 'e':  # Segunda letra 'e'
+                if cadena[2] == 'p':  # Tercera letra 'p'
+                    if cadena[3] == 'e':  # Cuarta letra 'e'
+                        if cadena[4] == 'a':  # Quinta letra 'a'
+                            if cadena[5] == 't':  # Sexta letra 't'
+                                return True  # Es palabra reservada "repeat"
+        else:
+            if cadena[0] == 'a':  # Primera letra 'a'
+                if cadena[1] == 's':  # Segunda letra 's'
+                    if cadena[2] == 's':  # Tercera letra 's'
+                        if cadena[3] == 'e':  # Cuarta letra 'e'
+                            if cadena[4] == 'r':  # Quinta letra 'r'
+                                if cadena[5] == 't':  # Sexta letra 't'
+                                    return True  # Es palabra reservada "assert"
+            else:
+                if cadena[0] == 'c':  # Primera letra 'c'
+                    if cadena[1] == 'l':  # Segunda letra 'l'
+                        if cadena[2] == 'a':  # Tercera letra 'a'
+                            if cadena[3] == 'u':  # Cuarta letra 'u'
+                                if cadena[4] == 's':  # Quinta letra 's'
+                                    if cadena[5] == 'e':  # Sexta letra 'e'
+                                        return True  # Es palabra reservada "clause"
+
+    # Palabras reservadas de longitud 7: "consult", "retract"
+    elif longitud == 7:
+        if cadena[0] == 'c':  # Primera letra 'c'
+            if cadena[1] == 'o':  # Segunda letra 'o'
+                if cadena[2] == 'n':  # Tercera letra 'n'
+                    if cadena[3] == 's':  # Cuarta letra 's'
+                        if cadena[4] == 'u':  # Quinta letra 'u'
+                            if cadena[5] == 'l':  # Sexta letra 'l'
+                                if cadena[6] == 't':  # Séptima letra 't'
+                                    return True  # Es palabra reservada "consult"
+        else:
+            if cadena[0] == 'r':  # Primera letra 'r'
+                if cadena[1] == 'e':  # Segunda letra 'e'
+                    if cadena[2] == 't':  # Tercera letra 't'
+                        if cadena[3] == 'r':  # Cuarta letra 'r'
+                            if cadena[4] == 'a':  # Quinta letra 'a'
+                                if cadena[5] == 'c':  # Sexta letra 'c'
+                                    if cadena[6] == 't':  # Séptima letra 't'
+                                        return True  # Es palabra reservada "retract"
+
+    return False  # No es palabra reservada
+
